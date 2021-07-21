620023a0dd81d65c11455424a9dd6fc132e4c8a0
Benedict <jokanolaadedipupo@gmail.com>
2021-07-21 12:15:41 +0100

    heroku

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,7 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         yield typeorm_1.createConnection({
             type: process.env.TYPEORM_CONNECTION,
             host: process.env.TYPEORM_HOST,
-            port: 5431,
+            port: process.env.TYPEORM_PORT,
             username: process.env.TYPEORM_USERNAME,
             password: process.env.TYPEORM_PASSWORD,
             database: process.env.TYPEORM_DATABASE,
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const main = async () => {
     await createConnection({
       type: process.env.TYPEORM_CONNECTION as "postgres",
       host: process.env.TYPEORM_HOST,
-      port: 5431,
+      port: process.env.TYPEORM_PORT as unknown as number,
       username: process.env.TYPEORM_USERNAME,
       password: process.env.TYPEORM_PASSWORD,
       database: process.env.TYPEORM_DATABASE,
